b0709821849921dfa2fd8495a6797c61f5025fe9
Nikita Teplyakov <>
2020-05-02 12:32:21 +0300

    setup frame for window

--- PokerMentor/AppDelegate.swift
+++ PokerMentor/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-	var window: UIWindow? = UIWindow()
+	var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		window?.rootViewController = UIViewController()
